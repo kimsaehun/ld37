48fe052f151f413f2728a8194287d618350cf3c9
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2016-12-11 09:11:38 -0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// set up canvas and context
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// IT'S A LION
+ctx.fillStyle = "#FDD835";
+ctx.fillRect(10, 10, 17, 17);
